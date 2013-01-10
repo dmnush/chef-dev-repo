081e674c54d30be967690e86b9724027222e94fe
dmnush <dm4252@att.com>
2013-01-10 13:44:39 -0500

    added opensssh cookbook from Chef server

---
+++ cookbooks/openssh-1.1.1/metadata.rb
@@ -0,0 +1,12 @@
+name              "openssh"
+maintainer        "Opscode, Inc."
+maintainer_email  "cookbooks@opscode.com"
+license           "Apache 2.0"
+description       "Installs openssh"
+version           "1.1.1"
+
+recipe "openssh", "Installs openssh"
+
+%w{ redhat centos fedora ubuntu debian arch scientific }.each do |os|
+  supports os
+end
